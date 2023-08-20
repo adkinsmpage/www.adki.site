cef59e6749ea0d24df16cbcf559b446d61728784
Adkimsm <adkinsm9277@gmail.com>
2023-08-20 17:48:15 +0800

    feat: button home

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,19 +1,25 @@
 import { useEffect } from 'react';
 import '../styles/App.css'
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
 
 function App() {
     useEffect(() => { document.title = "Adkimsm Home" })
 
     return (
-        <>
-            <h1 style={{display: 'none'}}>Adkimsm</h1>
+        <Stack spacing={2}>
+            <h1 style={{ display: 'none' }}>Adkimsm</h1>
             <Typography variant="h2" gutterBottom sx={{ fontWeight: 600 }}>
                 Hello,<br />I'm <mark className='hSqPQ'>Adkimsm</mark>
             </Typography>
             <Typography variant="subtitle1" gutterBottom>Front-end Developer / Open Sourceror / Blogger / Android Player</Typography>
             <Typography variant="subtitle2" gutterBottom>A Student from China</Typography>
-        </>
+            <Stack spacing={2} direction="row" >
+                <Button variant="contained" href="https://blog.adki.site">Blog</Button>
+                <Button variant="outlined" href="https://github.com/Adkimsm">GitHub</Button>
+            </Stack>
+        </Stack>
     )
 }
 
