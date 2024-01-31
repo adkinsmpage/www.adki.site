1e836071592b88d9412e745cc6038a17300cf4b6
Adkimsm <adkinsm9277@gmail.com>
2024-01-31 16:04:56 +0800

    fix: the categories's z-index

--- src/pages/posts.jsx
+++ src/pages/posts.jsx
@@ -40,7 +40,7 @@ function App() {
                             </time>
                             <a
                                 href={post.categories[0].permalink}
-                                className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 dark:bg-gray-800 dark:text-slate-300"
+                                className="relative rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 dark:bg-gray-800 dark:text-slate-300"
                             >
                                 {post.categories[0].name}
                             </a>
