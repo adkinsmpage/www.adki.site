e390257a60fa6d87a0a1a03bbda80c9449379b7e
Adkimsm <adkinsm9277@gmail.com>
2024-01-31 15:02:33 +0800

    style: better color for hovering nav link

--- src/pages/_layout.jsx
+++ src/pages/_layout.jsx
@@ -100,7 +100,7 @@ export default function Example({ children }) {
                                             data-cursor="block"
                                             key={item.name}
                                             to={item.href}
-                                            className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900  dark:text-slate-300 hover:bg-gray-50"
+                                            className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900  dark:text-slate-300 hover:bg-gray-900"
                                         >
                                             {item.name}
                                         </Link>
@@ -109,7 +109,7 @@ export default function Example({ children }) {
                                 <div className="py-6">
                                     <Link
                                         to="#"
-                                        className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 dark:text-slate-300 hover:bg-gray-50"
+                                        className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 dark:text-slate-300 hover:bg-gray-900"
                                     >
                                         <MoonIcon className="w-6 h-6" onClick={() => setDarkMode(mode => !mode)} />
                                     </Link>
