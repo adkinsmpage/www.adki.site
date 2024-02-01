ba3cad31f4128add832b095bf06442ac4d679379
Adkimsm <adkinsm9277@gmail.com>
2024-02-01 19:32:51 +0800

    style: add transition for sidebar

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -1,8 +1,8 @@
 'use client'
 
 import Link from 'next/link'
-import { useState, useEffect } from 'react'
-import { Dialog } from '@headlessui/react'
+import { useState, useEffect, Fragment } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { MoonIcon } from '@heroicons/react/24/solid'
 
@@ -59,7 +59,7 @@ export default function HeaderNav({ children }) {
                         </button>
                     </div>
                     <div className="hidden lg:flex lg:gap-x-12">
-                        { navigations.map((item) => (
+                        {navigations.map((item) => (
                             <Link key={item.name} href={item.href} data-cursor="block" className="px-3.5 py-2.5 text-sm font-semibold leading-6 text-gray-900  dark:text-slate-300">
                                 {item.name}
                             </Link>
@@ -74,58 +74,82 @@ export default function HeaderNav({ children }) {
                         </button>
                     </div>
                 </nav>
-                <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-                    <div className="fixed inset-0" />
-                    <Dialog.Panel className="fixed inset-y-0 right-0 w-full overflow-y-auto bg-white dark:bg-gray-950 dark:text-slate-300 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-                        <div className="flex items-center justify-between">
-                            <Link
-                                onClick={() => setMobileMenuOpen(false)}
-                                href="/"
-                                className="-m-1.5 p-1.5"
-                                data-cursor="block">
-                                <span className="sr-only">Adkimsm</span>
-                                <img
-                                    className="h-8 w-auto"
-                                    src="http://q1.qlogo.cn/g?b=qq&nk=3248546325&s=100"
-                                    alt=""
-                                />
-                            </Link>
-                            <button
-                                type="button"
-                                className="-m-2.5 rounded-md p-2.5 text-gray-700 dark:text-slate-300"
-                                onClick={() => setMobileMenuOpen(false)}
-                            >
-                                <span className="sr-only">Close menu</span>
-                                <XMarkIcon className="h-6 w-6" aria-hidden="true" />
-                            </button>
-                        </div>
-                        <div className="mt-6 flow-root">
-                            <div className="-my-6 divide-y divide-gray-500/10">
-                                <div className="space-y-2 py-6">
-                                    { navigations.map((item) => (
-                                        <Link
-                                            data-cursor="block"
-                                            key={item.name}
-                                            href={item.href}
-                                            onClick={() => setMobileMenuOpen(false)}
-                                            className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900  dark:text-slate-300 dark:hover:bg-gray-900 hover:bg-gray-50"
-                                        >
-                                            {item.name}
-                                        </Link>
-                                    ))}
-                                </div>
-                                <div className="py-6">
+                <Transition
+                    enter="transition-opacity duration-75"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="transition-opacity duration-150"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                    show={mobileMenuOpen}
+                    as={Fragment}
+                >
+                    <Dialog as="div" className="lg:hidden" onClose={setMobileMenuOpen}>
+                        <Transition.Child
+                            as={Fragment}
+                        >
+                            <div className="fixed inset-0" />
+                        </Transition.Child>
+                        <Transition.Child
+                            as={Fragment}
+                            enter="ease-out duration-300"
+                            enterFrom="opacity-0 scale-95"
+                            enterTo="opacity-100 scale-100"
+                            leave="ease-in duration-200"
+                            leaveFrom="opacity-100 scale-100"
+                            leaveTo="opacity-0 scale-95"
+                        >
+                            <Dialog.Panel className="fixed inset-y-0 right-0 w-full overflow-y-auto bg-white dark:bg-gray-950 dark:text-slate-300 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+                                <div className="flex items-center justify-between">
                                     <button
-                                        onClick={() => setDarkMode(mode => !mode)}
-                                        className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 dark:text-slate-300 dark:hover:bg-gray-900 hover:bg-gray-50"
+                                        onClick={() => setMobileMenuOpen(false)}
+                                        className="-m-1.5 p-1.5"
+                                        data-cursor="block">
+                                        <span className="sr-only">Adkimsm</span>
+                                        <img
+                                            className="h-8 w-auto"
+                                            src="http://q1.qlogo.cn/g?b=qq&nk=3248546325&s=100"
+                                            alt=""
+                                        />
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="-m-2.5 rounded-md p-2.5 text-gray-700 dark:text-slate-300"
+                                        onClick={() => setMobileMenuOpen(false)}
                                     >
-                                        <MoonIcon className="w-6 h-6" />
+                                        <span className="sr-only">Close menu</span>
+                                        <XMarkIcon className="h-6 w-6" aria-hidden="true" />
                                     </button>
                                 </div>
-                            </div>
-                        </div>
-                    </Dialog.Panel>
-                </Dialog>
+                                <div className="mt-6 flow-root">
+                                    <div className="-my-6 divide-y divide-gray-500/10">
+                                        <div className="space-y-2 py-6">
+                                            {navigations.map((item) => (
+                                                <Link
+                                                    data-cursor="block"
+                                                    key={item.name}
+                                                    href={item.href}
+                                                    onClick={() => setMobileMenuOpen(false)}
+                                                    className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900  dark:text-slate-300 dark:hover:bg-gray-900 hover:bg-gray-50"
+                                                >
+                                                    {item.name}
+                                                </Link>
+                                            ))}
+                                        </div>
+                                        <div className="py-6">
+                                            <button
+                                                onClick={() => setDarkMode(mode => !mode)}
+                                                className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 dark:text-slate-300 dark:hover:bg-gray-900 hover:bg-gray-50"
+                                            >
+                                                <MoonIcon className="w-6 h-6" />
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Dialog.Panel>
+                        </Transition.Child>
+                    </Dialog>
+                </Transition>
             </header>
 
             <div className="relative isolate px-6 pt-14 lg:px-8 -z-10">
