754842acc6ad4d5996b11d9c1ad494b7ccfd244e
Adkimsm <adkinsm9277@gmail.com>
2024-06-24 21:11:46 +0800

    feat(postlist): add super-link for post list

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -19,45 +19,49 @@ export default async function Page() {
             <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-2">
                 <Suspense fallback={<Loading />}>
                     {data.fms.map((post, i) => (
-                        <div
+                        <Link
                             key={i}
-                            className="overflow-hidden rounded-xl border border-gray-200 bg-gray-50/45 transition-all hover:scale-[1.03] ease-linear hover:shadow-[0_30px_60px_-15px_rgba(0,0,0,0.17)]"
+                            href={`/posts/${data.titles.at(i)}`}
                         >
-                            <div className="flex items-center gap-x-4 border-b border-gray-900/5 p-6">
-                                <div className="text-sm font-medium leading-6 text-gray-900">
-                                    {post.title}
+                            <div
+                                className="overflow-hidden rounded-xl border border-gray-200 bg-gray-50/45 transition-all hover:scale-[1.03] ease-linear hover:shadow-[0_30px_60px_-15px_rgba(0,0,0,0.17)]"
+                            >
+                                <div className="flex items-center gap-x-4 border-b border-gray-900/5 p-6">
+                                    <div className="text-sm font-medium leading-6 text-gray-900">
+                                        {post.title}
+                                    </div>
                                 </div>
+                                <dl className="-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6">
+                                    <div className="flex justify-between gap-x-4 py-3">
+                                        <dt className="text-gray-500">标签</dt>
+                                        <dd className="flex items-start gap-x-2">{
+                                            post.tags.map((tag, i) => (
+                                                <span key={i}
+                                                    className="inline-flex items-center rounded-md bg-gray-100 px-2 py-1 text-xs font-medium text-gray-600"
+                                                >
+                                                    {tag}
+                                                </span>
+                                            ))}
+                                        </dd>
+                                    </div>
+                                    <div className="flex justify-between gap-x-4 py-3">
+                                        <dt className="text-gray-500">分类</dt>
+                                        <dd className="flex items-start gap-x-2">
+                                            <div
+                                                className="inline-flex items-center rounded-md bg-gray-100 px-2 py-1 text-xs font-medium text-gray-600">
+                                                {post.categories.at(0)}
+                                            </div>
+                                        </dd>
+                                    </div>
+                                    <div className="flex justify-between gap-x-4 py-3">
+                                        <dt className="text-gray-500">最新更新时间</dt>
+                                        <dd className="text-gray-700">
+                                            <span>{dayjs(post.date).format("YYYY-MM-DD")}</span>
+                                        </dd>
+                                    </div>
+                                </dl>
                             </div>
-                            <dl className="-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6">
-                                <div className="flex justify-between gap-x-4 py-3">
-                                    <dt className="text-gray-500">标签</dt>
-                                    <dd className="flex items-start gap-x-2">{
-                                        post.tags.map((tag, i) => (
-                                            <span key={i}
-                                                className="inline-flex items-center rounded-md bg-gray-100 px-2 py-1 text-xs font-medium text-gray-600"
-                                            >
-                                                {tag}
-                                            </span>
-                                        ))}
-                                    </dd>
-                                </div>
-                                <div className="flex justify-between gap-x-4 py-3">
-                                    <dt className="text-gray-500">分类</dt>
-                                    <dd className="flex items-start gap-x-2">
-                                        <div
-                                            className="inline-flex items-center rounded-md bg-gray-100 px-2 py-1 text-xs font-medium text-gray-600">
-                                            {post.categories.at(0)}
-                                        </div>
-                                    </dd>
-                                </div>
-                                <div className="flex justify-between gap-x-4 py-3">
-                                    <dt className="text-gray-500">最新更新时间</dt>
-                                    <dd className="text-gray-700">
-                                        <span>{dayjs(post.date).format("YYYY-MM-DD")}</span>
-                                    </dd>
-                                </div>
-                            </dl>
-                        </div>
+                        </Link>
                     ))}</Suspense>
             </div>
         </div>
