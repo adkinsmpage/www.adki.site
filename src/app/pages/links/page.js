962d1d8e4b818320fd44327263e3dc7e72332d1d
Adkimsm <adkinsm9277@gmail.com>
2024-06-29 22:31:44 +0800

    feat(links): new links page style
    thanks for [Eltrac](https://www.guhub.cn/links)

--- src/app/pages/links/page.js
+++ src/app/pages/links/page.js
@@ -1,31 +1,42 @@
-"use client"
-
-import { HoverEffect } from "@/components/hoverCard";
-
 export default function () {
   return <div className="mx-auto relative isolate overflow-hidden py-12 sm:py-12 lg:overflow-visible px-7 flex items-center flex-col">
-      <div>
-        <div className="pt-6 pb-6 mb-0">
-          <h1
-            data-cursor="block"
-            className="mb-8 text-3xl font-extrabold leading-9 text-gray-900 dark:text-white sm:text-4xl sm:leading-10 md:text-5xl md:leading-14"
+    <div>
+      <div className="pt-6 pb-6 mb-0">
+        <h1
+          data-cursor="block"
+          className="mb-8 text-3xl font-extrabold leading-9 text-gray-900 dark:text-white sm:text-4xl sm:leading-10 md:text-5xl md:leading-14"
+        >
+          朋友们
+        </h1>
+      </div>
+      <div className="mb-8 w-full">
+        <div>
+          <div
+            className="markdown-body text-base leading-7 text-gray-700 dark:text-slate-300 px-0"
           >
-            朋友们
-          </h1>
-        </div>
-        <div className="mb-8 w-full">
-          <div>
-            <div
-              className="markdown-body text-base leading-7 text-gray-700 dark:text-slate-300 px-0"
-            >
-              <div className="links max-w-5xl mx-auto px-8">
-                <HoverEffect items={projects} />
+            <div className="links max-w-5xl mx-auto px-8">
+                <ul className="flex flex-wrap gap-5">
+                  {projects.map((link, i) => 
+                  <li key={i} className="transition-all border rounded-2xl px-4 hover:border-transparent hover:shadow-[0_30px_60px_-15px_rgba(0,0,0,0.17)]">
+                    <a href={link.link} target="_blank" className="block flex items-center gap-3">
+                      <img
+                        alt="avatar of Ohmyga" loading="lazy" width="100" height="100" decoding="async" data-nimg="1"
+                        className="hover:rotate-[360deg] inline-block w-8 rounded-full"
+                        src={link.img}
+                      />
+                      <span className="font-bold">{link.title}</span>
+                      <span>|</span>
+                      <span className="font-bold">{link.description}</span>
+                    </a>
+                  </li>
+                )}
+                </ul>
               </div>
-            </div>
           </div>
         </div>
       </div>
     </div>
+  </div>
 }
 
 export const projects = [
