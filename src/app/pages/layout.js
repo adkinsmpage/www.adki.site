a5d7ca5accad6125c8a6deab6ffaa350975a6830
Adkimsm <adkinsm9277@gmail.com>
2024-07-01 21:14:33 +0800

    style(pages-layout): remove comments of page layout

--- src/app/pages/layout.js
+++ src/app/pages/layout.js
@@ -4,15 +4,6 @@ import '@/assets/css/markdown.css'
 export default async function Page({ children }) {
     return <div className="mx-auto relative isolate overflow-hidden py-12 sm:py-12 lg:overflow-visible px-7 flex items-center flex-col">
             <div>
-                {/*<div className="pt-6 pb-6 mb-0">
-                    <h1
-                        data-cursor="block"
-                        className="mb-8 text-3xl font-extrabold leading-9 text-gray-900 dark:text-white sm:text-4xl sm:leading-10 md:text-5xl md:leading-14"
-                    >
-                        {data.fms.title}
-                    </h1>
-                    <time className='opacity-50 !-mt-6 slide-enter-50'>{dayjs(data.fms.date).format("YYYY-MM-DD")}</time>
-                </div>*/}
                 <div className="mb-8 w-full">
                     <div>
                         <div
